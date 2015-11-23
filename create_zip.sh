454d151981dbe8e847bc36c9da2ee07e9a84870a
vmarchak <viktor.marchak@gmail.com>
2015-11-23 10:24:04 +0200

    init

---
+++ create_zip.sh
@@ -0,0 +1,5 @@
+rm goodgame.tv.plugin.zip
+rm ~/plugin.video.goodgame-0.5.zip
+rm goodgame.tv.plugin/wrappers.pyc
+zip -9 -r goodgame.tv.plugin.zip goodgame.tv.plugin
+cp goodgame.tv.plugin.zip ~/goodgame.tv.plugin-0.5.zip
